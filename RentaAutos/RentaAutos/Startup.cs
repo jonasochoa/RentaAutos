068a4dbd3a98b5f8414d1ad5db8d31700b902744
Jonas <jonasochoa2@hotmail.com>
2017-04-07 07:27:02 -0500

    Jonatan: Creacion de Trabajo

---
+++ RentaAutos/RentaAutos/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentaAutos.Startup))]
+namespace RentaAutos
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
